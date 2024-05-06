a27aed3d8ce3a50555fd3c0dd15fefe0cf183633
Alex-Davo <139930250+Alex-Davo@users.noreply.github.com>
2024-05-06 00:50:54 +0000

    fix WordleSolver.cpp comment

--- WordleBot/WordleSolver.cpp
+++ WordleBot/WordleSolver.cpp
@@ -2,7 +2,7 @@
 
 //This is the main function. It is used to start the program and initialize some variables. It has the general framework of the code.
 //To keep the main clean and organized, wordleAlgo is where most of the search algorithm happens, and wordleInpOut is where most of the 
-//I/O is happening. To compile, all three .cpp files and the header WordleSolver.h are needed.
+//I/O is happening. To compile, all three .cpp files, the header WordleSolver.h, and the word.txt and Wordle_words.txt are needed.
 int main() {
     bool gameOver = 0;
     int counter = 0;
